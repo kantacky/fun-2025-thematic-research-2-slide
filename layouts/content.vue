86f14a827ccdd991e737bdb1655845bd091fc308
Kanta Oikawa <iam@kantacky.com>
2026-01-23 08:23:25 +0900

    Refactor content layout to include dynamic subtitle and enhanced styling

--- layouts/content.vue
+++ layouts/content.vue
@@ -1,17 +1,56 @@
-<style>
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-  margin: 0.8em 0em 0.3em 0em;
-  font-weight: bold;
-}
-</style>
+<script setup>
+defineProps({
+  subtitle: String | undefined,
+});
+</script>
 
 <template>
-  <div class="slidev-layout default">
+  <div class="slidev-layout content">
+    <div class="header">
+      <div class="title" v-if="subtitle && $frontmatter.title">
+        {{ $frontmatter.title }}
+      </div>
+      <div class="subtitle" v-if="subtitle || $frontmatter.title">
+        {{ subtitle ?? $frontmatter.title }}
+      </div>
+    </div>
     <slot />
   </div>
 </template>
+
+<style>
+.slidev-layout.content {
+  @apply my-4;
+
+  .header {
+    @apply flex flex-col gap-1;
+
+    .title {
+      @apply text-lg font-bold;
+    }
+
+    .subtitle {
+      @apply text-3xl font-bold;
+    }
+  }
+
+  .split {
+    @apply flex flex-row gap-4;
+    > div {
+      @apply flex-1;
+    }
+  }
+
+  li {
+    @apply my-1;
+  }
+
+  table {
+    @apply my-8;
+    th,
+    td {
+      @apply border-b border-gray-400;
+    }
+  }
+}
+</style>
